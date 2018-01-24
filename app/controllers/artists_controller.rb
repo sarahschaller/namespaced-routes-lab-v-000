7eb792281a0ecf45907de2dc7133550ae282fc3b
Sarah Schaller <sarcsch@aol.com>
2018-01-24 18:07:01 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,10 +8,10 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    if Preferences.allow_create_artists == true 
-      @artist = Artist.new 
-    else 
-      redirect_to artists_path 
+    if Preferences.allow_create_artists == true
+      @artist = Artist.new
+    else
+      redirect_to artists_path
     end
   end
 
